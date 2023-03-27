efdcd1312bc17dda1e8dd18e07553eab4ec66a16
LucetTin5 <lucettin5@gmail.com>
2023-03-27 15:09:10 +0900

    setting: add chakra ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,13 +6,16 @@ import {
 import type { AppProps } from "next/app";
 import { useState } from "react";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ChakraProvider } from "@chakra-ui/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
   return (
     <QueryClientProvider client={queryClient}>
       <Hydrate {...pageProps}>
-        <Component {...pageProps} />
+        <ChakraProvider>
+          <Component {...pageProps} />
+        </ChakraProvider>
       </Hydrate>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
