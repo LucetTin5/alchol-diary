4afe50ebfd0b13daab0dc0968d4e5ee1c2690af6
LucetTin5 <lucettin5@gmail.com>
2023-04-05 16:29:32 +0900

    fix: env fix

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.AXIOS_BASE,
+  baseURL: process.env.NEXT_PUBLIC_AXIOS_BASE,
 });
 
 export default axiosInstance;
