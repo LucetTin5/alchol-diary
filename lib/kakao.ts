4e3a1574ebd1098300af008d4e9350d238b2f9e8
LucetTin5 <lucettin5@gmail.com>
2023-04-06 13:22:45 +0900

    fix: code review 개선점 반영

--- lib/kakao.ts
+++ lib/kakao.ts
@@ -1,6 +1,6 @@
 const REST_API_KEY = process.env.NEXT_PUBLIC_REST_API_KEY;
-const REDIRECT_URI = process.env.NEXT_PUBLIC_VERCEL_URL
-  ? process.env.NEXT_PUBLIC_VERCEL_URL + "/users/kakao/callback"
-  : "http://localhost:3000/users/kakao/callback";
+const REDIRECT_URI =
+  process.env.NEXT_PUBLIC_VERCEL_URL?.concat("/users/kakao/callback") ??
+  "http://localhost:3000/users/kakao/callback";
 
 export const KAKAO_AUTH_URI = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
